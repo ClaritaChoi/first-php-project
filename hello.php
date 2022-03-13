b66a44d035f2fc8069a97d71874497719333aa28
Jisun Choi <jisun@sungil-i.kr>
2022-03-13 22:26:07 +0900

    first commit

---
+++ hello.php
@@ -0,0 +1,12 @@
+<?php
+    echo "안녕하세요";
+    echo "hello";
+    //
+    /* */
+
+    echo "<h2>php공부</h2>";
+    echo "쉽다<br>";
+    echo "재미있다!<br>";
+    echo "<img src='img/dog.jpeg' width'100'>"
+
+?>
